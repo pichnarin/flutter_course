d2dd5174d1476d10bfc640d7d7eb2652d0e99021
noobmaster69 <139727372+pichnarin@users.noreply.github.com>
2024-10-17 12:45:06 +0700

    level 4 added

--- Challenge/level4.dart
+++ Challenge/level4.dart
@@ -4,18 +4,14 @@ class Distance {
   // Private constructor
   const Distance._(this._meters);
 
-  // Factory constructors
-  factory Distance.fromCms(double cms) {
-    return Distance._(cms / 100);
-  }
+  // name constructors
+  //use when input in different unit (cms, meters, kms) but finally return in meters
 
-  factory Distance.fromMeters(double meters) {
-    return Distance._(meters);
-  }
+  Distance.fromCms(double cms) : _meters = cms / 100;
 
-  factory Distance.fromKms(double kms) {
-    return Distance._(kms * 1000);
-  }
+  Distance.fromMeters(double meters) : _meters = meters;
+
+  Distance.fromKms(double kms) : _meters = kms * 1000 ;
 
   // Getter methods
   double get inCms => _meters * 100;
@@ -35,12 +31,14 @@ class Distance {
 
 void main() {
 
+  //user input in different unit but its convert to meters
+
   var distance1 = Distance.fromCms(150); // 1.5 meters
   var distance2 = Distance.fromMeters(2); // 2 meters
   var distance3 = Distance.fromKms(0.005); // 5 meters
-
   var totalDistance = distance1.add(distance2).add(distance3);
 
+  //convert back the totalDistance in meters to (cms, kms, meters)
   print('Total Distance: $totalDistance');
   print('In cms: ${totalDistance.inCms}');
   print('In meters: ${totalDistance.inMeters}');
